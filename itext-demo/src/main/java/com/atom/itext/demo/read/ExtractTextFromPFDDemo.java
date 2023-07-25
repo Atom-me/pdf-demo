489e7fcc682c23296fd7464d29902867460f5886
Atom <sarming@126.com>
2023-07-25 14:40:12 +0800

    extract text from pdf

---
+++ itext-demo/src/main/java/com/atom/itext/demo/read/ExtractTextFromPFDDemo.java
@@ -0,0 +1,24 @@
+package com.atom.itext.demo.read;
+
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.parser.PdfTextExtractor;
+
+import java.io.IOException;
+
+/**
+ * @author Atom
+ */
+public class ExtractTextFromPFDDemo {
+    public static void main(String[] args) throws IOException {
+        PdfReader reader = new PdfReader("sample.pdf");
+        int pages = reader.getNumberOfPages();
+        StringBuilder text = new StringBuilder();
+        for (int i = 1; i <= pages; i++) {
+            text.append(PdfTextExtractor.getTextFromPage(reader, i));
+        }
+        reader.close();
+
+        System.err.println(text);
+
+    }
+}
