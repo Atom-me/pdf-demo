b9066508bc4a4ef7c92aeab367eaf85766d2a33e
Atom <sarming@126.com>
2023-07-25 14:06:41 +0800

    pdfbox create pdf insert image

---
+++ pdfbox-demo/src/main/java/com/atom/pdfbox/demo/InsertImageInPDFDemo.java
@@ -0,0 +1,34 @@
+package com.atom.pdfbox.demo;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * 创建一个新的PDF文件，并往里面插入图片
+ *
+ * @author Atom
+ */
+public class InsertImageInPDFDemo {
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        PDDocument document = new PDDocument();
+        PDPage page = new PDPage();
+        document.addPage(page);
+
+        Path path = Paths.get(ClassLoader.getSystemResource("demo001.png").toURI());
+        PDPageContentStream contentStream = new PDPageContentStream(document, page);
+        PDImageXObject image = PDImageXObject.createFromFile(path.toAbsolutePath().toString(), document);
+        contentStream.drawImage(image, 80, 80);
+        contentStream.close();
+
+        document.save("pdfBoxImage.pdf");
+        document.close();
+
+    }
+}
