d415b6f600a151fb35a1b1f030517c27855f5c11
Atom <sarming@126.com>
2023-07-25 17:23:29 +0800

    itext7 insert image

---
+++ itext7-demo/src/test/java/com/atom/itext7/Itext7DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atom.itext7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Itext7DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
