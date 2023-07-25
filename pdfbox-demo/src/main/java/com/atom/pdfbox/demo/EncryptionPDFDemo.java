d53c92ed1a891cb6bad91c2c5ee662899ce634d1
Atom <sarming@126.com>
2023-07-25 14:11:21 +0800

    pdfbox encryption pdf file

---
+++ pdfbox-demo/src/main/java/com/atom/pdfbox/demo/EncryptionPDFDemo.java
@@ -0,0 +1,48 @@
+package com.atom.pdfbox.demo;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.encryption.AccessPermission;
+import org.apache.pdfbox.pdmodel.encryption.StandardProtectionPolicy;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+
+import java.io.IOException;
+
+/**
+ * PDF文件加密，设置（打印）权限
+ * 两个密码：
+ * 1. 普通用户密码
+ * 2. 拥有者密码，使用拥有者密码打开已加密的PDF文件拥有所有权限，包含打印权限。
+ *
+ * @author Atom
+ */
+public class EncryptionPDFDemo {
+    public static void main(String[] args) throws IOException {
+        PDDocument document = new PDDocument();
+        PDPage page = new PDPage();
+        document.addPage(page);
+
+        PDPageContentStream contentStream = new PDPageContentStream(document, page);
+
+        contentStream.setFont(PDType1Font.COURIER, 18);
+        // 添加文本
+        contentStream.beginText();
+        contentStream.newLineAtOffset(100, 660);
+        contentStream.showText("hello line3");
+        contentStream.endText();
+
+        contentStream.close();
+
+        AccessPermission accessPermission = new AccessPermission();
+        accessPermission.setCanPrint(false);
+        accessPermission.setCanModify(false);
+
+        StandardProtectionPolicy standardProtectionPolicy
+                = new StandardProtectionPolicy("ownerpass", "userpass", accessPermission);
+        document.protect(standardProtectionPolicy);
+        document.save("pdfBoxEncryption.pdf");
+        document.close();
+
+    }
+}
