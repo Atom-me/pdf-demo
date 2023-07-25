d771a334643019e9f99135453aa617fd31050664
Atom <sarming@126.com>
2023-07-25 19:30:06 +0800

    itext7 insert table

--- itext7-demo/src/main/java/com/atom/itext7/demo/write/InsertTableInPDFDemo.java
+++ itext7-demo/src/main/java/com/atom/itext7/demo/write/InsertTableInPDFDemo.java
@@ -1,8 +1,8 @@
 package com.atom.itext7.demo.write;
 
-
-import com.itextpdf.io.font.PdfEncodings;
 import com.itextpdf.io.font.constants.StandardFonts;
+import com.itextpdf.kernel.colors.Color;
+import com.itextpdf.kernel.colors.DeviceRgb;
 import com.itextpdf.kernel.font.PdfFont;
 import com.itextpdf.kernel.font.PdfFontFactory;
 import com.itextpdf.kernel.geom.PageSize;
@@ -17,8 +17,6 @@ import com.itextpdf.layout.properties.UnitValue;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.util.StringTokenizer;
 
 /**
  * 创建一个新的PDF文件，并往里面插入表哥
@@ -26,43 +24,49 @@ import java.util.StringTokenizer;
  * @author Atom
  */
 public class InsertTableInPDFDemo {
-    public static void main(String[] args) throws IOException, URISyntaxException {
+    private static final Color HEADER_BG_COLOR = new DeviceRgb(166, 203, 11);
+    private static final Color LINE_COLOR = new DeviceRgb(204, 204, 204);
+
+    public static void main(String[] args) throws IOException {
         PdfWriter writer = new PdfWriter("/Users/atom/work/workspace/pdf-demo/itext7-demo/sampleTablePDF.pdf");
         PdfDocument pdf = new PdfDocument(writer);
-        Document document = new Document(pdf, PageSize.A4.rotate());//设置页面大小，rotate（）表示页面横向
-        document.setMargins(20, 20, 20, 20);//设置页边距
-        PdfFont font = PdfFontFactory.createFont(StandardFonts.HELVETICA);//定义不同的字体类型
-        PdfFont bold = PdfFontFactory.createFont(StandardFonts.HELVETICA_BOLD);//定义不同的字体类型
-        Table table = new Table(new float[]{4, 1, 3, 4, 3, 3, 3, 3, 1});//每个数定义一个列的相对宽度。
-        table.setWidth(UnitValue.createPercentValue(100));//表的宽度相对于页面的可用宽度，在这种情况下，表将使用100% 的页面宽度，减去页边距。
-        //接下来的操作是通过读取一个csv文件里的内容填充到pdf的表格上
-        BufferedReader br = new BufferedReader(new FileReader("/Users/atom/work/workspace/pdf-demo/itext7-demo/src/main/resources/file/data.csv"));
-        String line = br.readLine();
-        //process ()方法，使用特定的字体将行添加到表中，并定义行是否包含标题行的内容。
-        process(table, line, bold, true);
-        while ((line = br.readLine()) != null) {
-            process(table, line, font, false);
+        // 设置页面大小，rotate（）表示页面横向
+        Document document = new Document(pdf, PageSize.A4.rotate());
+        // 设置页边距
+        document.setMargins(20, 20, 20, 20);
+        // 定义不同的字体类型
+        PdfFont font = PdfFontFactory.createFont(StandardFonts.HELVETICA);
+        PdfFont bold = PdfFontFactory.createFont(StandardFonts.HELVETICA_BOLD);
+        // 每个数定义一个列的相对宽度。
+        Table table = new Table(new float[]{4, 1, 3, 4, 3, 3, 3, 3, 1});
+        // 表的宽度相对于页面的可用宽度，在这种情况下，表将使用100% 的页面宽度，减去页边距。
+        table.setWidth(UnitValue.createPercentValue(100));
+        // 接下来的操作是通过读取一个csv文件里的内容填充到pdf的表格上
+        try (BufferedReader br = new BufferedReader(new FileReader("/Users/atom/work/workspace/pdf-demo/itext7-demo/src/main/resources/file/data.csv"))) {
+            String line = br.readLine();
+            // process ()方法，使用特定的字体将行添加到表中，并定义行是否包含标题行的内容。
+            process(table, line, bold, true);
+            while ((line = br.readLine()) != null) {
+                process(table, line, font, false);
+            }
         }
-        br.close();
         document.add(table);
         document.close();
-
     }
 
     public static void process(Table table, String line, PdfFont font, boolean isHeader) {
-        StringTokenizer tokenizer = new StringTokenizer(line, ";");
-        while (tokenizer.hasMoreTokens()) {
+        String[] tokens = line.split(";");
+        for (String token : tokens) {
+            Cell cell = new Cell()
+                    .setFont(font)
+                    .add(new Paragraph(token))
+                    .setBackgroundColor(isHeader ? HEADER_BG_COLOR : LINE_COLOR);
+
             if (isHeader) {
-                table.addHeaderCell(
-                        new Cell().add(
-                                new Paragraph(tokenizer.nextToken()).setFont(font)));
+                table.addHeaderCell(cell);
             } else {
-                table.addCell(
-                        new Cell().add(
-                                new Paragraph(tokenizer.nextToken()).setFont(font)));
+                table.addCell(cell);
             }
         }
     }
-
-
 }
