850ffd060ee4b08ba760f027c5d7d3d1232ab5d5
Atom <sarming@126.com>
2023-07-26 16:01:40 +0800

    html to pdf

--- pdfbox-demo/src/main/java/com/atom/pdfbox/convert/PDF2HTMLExample.java
+++ pdfbox-demo/src/main/java/com/atom/pdfbox/convert/PDF2HTMLExample.java
@@ -15,11 +15,11 @@ import java.io.Writer;
  * @author Atom
  */
 public class PDF2HTMLExample {
-    private static final String PDF = "/Users/atom/work/workspace/pdf-demo/pdfbox-demo/src/main/resources/pdf.pdf";
+    private static final String SOURCE_PDF = "./pdfbox-demo/src/main/resources/pdf.pdf";
 
     public static void main(String[] args) {
         try {
-            generateHTMLFromPDF(PDF);
+            generateHTMLFromPDF(SOURCE_PDF);
         } catch (IOException | ParserConfigurationException e) {
             e.printStackTrace();
         }
@@ -28,7 +28,7 @@ public class PDF2HTMLExample {
     private static void generateHTMLFromPDF(String filename) throws ParserConfigurationException, IOException {
         PDDocument pdf = PDDocument.load(new File(filename));
         PDFDomTree parser = new PDFDomTree();
-        Writer output = new PrintWriter("pdf.html", "utf-8");
+        Writer output = new PrintWriter("./pdfbox-demo/pdf.html", "utf-8");
         parser.writeText(pdf, output);
         output.close();
         if (pdf != null) {
