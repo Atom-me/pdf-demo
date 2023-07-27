935e4834850a4c25db04c0e052d818d1ea97e5e2
Atom <sarming@126.com>
2023-07-27 21:58:53 +0800

    解决itext7中文显示问题

--- itext7-demo/src/main/java/com/atom/itext7/convert/Html2PdfExample.java
+++ itext7-demo/src/main/java/com/atom/itext7/convert/Html2PdfExample.java
@@ -2,6 +2,9 @@ package com.atom.itext7.convert;
 
 import com.itextpdf.html2pdf.ConverterProperties;
 import com.itextpdf.html2pdf.HtmlConverter;
+import com.itextpdf.io.font.PdfEncodings;
+import com.itextpdf.layout.font.FontProvider;
+import com.itextpdf.layout.font.FontSet;
 
 import java.io.*;
 
@@ -15,6 +18,12 @@ public class Html2PdfExample {
         File pdfDest = new File("./itext7-demo/output/html.pdf");
         // pdfHTML specific code
         ConverterProperties converterProperties = new ConverterProperties();
+        //字体设置，解决中文不显示问题
+        FontSet fontSet = new FontSet();
+        // 加载自定义字体
+        fontSet.addFont(Html2PdfExample.class.getClassLoader().getResource("font/Alibaba-PuHuiTi-Regular.otf").getPath(), PdfEncodings.IDENTITY_H);
+        FontProvider fontProvider = new FontProvider(fontSet);
+        converterProperties.setFontProvider(fontProvider);
         HtmlConverter.convertToPdf(new FileInputStream(htmlSource), new FileOutputStream(pdfDest), converterProperties);
     }
 }
