1bb1e483e340858ffc9e5218dab8d767b483156d
Atom <sarming@126.com>
2023-07-27 14:58:16 +0800

    获取PDF 元数据信息

--- itext7-demo/src/main/java/com/atom/itext7/demo/read/PdfInfoIText7App.java
+++ itext7-demo/src/main/java/com/atom/itext7/demo/read/PdfInfoIText7App.java
@@ -10,8 +10,8 @@ public class PdfInfoIText7App {
     private static final String SOURCE_PDF = "./itext7-demo/src/main/resources/MultiPages-encryped.pdf";
 
     public static void main(String[] args) throws IOException {
-//        System.err.println(PdfInfoIText7.getNumberOfPages(SOURCE_PDF));
+        System.err.println(PdfInfoIText7.getNumberOfPages(SOURCE_PDF));
         System.err.println(PdfInfoIText7.isPasswordRequired(SOURCE_PDF));
-//        System.err.println(PdfInfoIText7.getInfo(SOURCE_PDF));
+        System.err.println(PdfInfoIText7.getInfo(SOURCE_PDF));
     }
 }
